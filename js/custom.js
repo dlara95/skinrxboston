95aa333baa30b0bba826cec88f8f05a823579d4c
Dayana <dayanalara95@gmail.com>
2017-07-05 17:03:00 -0600

    Remove alert on load

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 $ = jQuery;
 
 $(document).ready(function(){
-  alert('Hola');
  $('.scroll-btn > a').click(function() {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
